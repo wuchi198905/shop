b1bfb1b28d0dac2c5e387a3996bbed58b6083299
倔强的萝卜 <769729374@qq.com>
2020-11-04 10:08:07 +0800

    搭建框架，第一次上传

--- src/main/java/com/example/shop/management/mapper/LeUserMapper.java
+++ src/main/java/com/example/shop/management/mapper/LeUserMapper.java
@@ -2,6 +2,8 @@ package com.example.shop.management.mapper;
 
 import com.example.shop.management.bean.LeUser;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +13,8 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * @author 陈志浩123
  * @since 2020-11-03
  */
+@Mapper
+@Repository
 public interface LeUserMapper extends BaseMapper<LeUser> {
 
 }
