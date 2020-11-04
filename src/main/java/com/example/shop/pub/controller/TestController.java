1c358d89de747c9865ede9fee3f8231e23de5dfc
倔强的萝卜 <769729374@qq.com>
2020-11-04 15:47:48 +0800

    增加web

--- src/main/java/com/example/shop/pub/controller/TestController.java
+++ src/main/java/com/example/shop/pub/controller/TestController.java
@@ -2,16 +2,11 @@ package com.example.shop.pub.controller;
 
 
 
-import com.alibaba.fastjson.JSONObject;
 import com.example.shop.base.rabbitmq.MessageProducer;
-import com.example.shop.base.web.WebSocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +16,7 @@ public class TestController {
 
     @Autowired
     private MessageProducer messageProducer;
-    private WebSocket websocket = new WebSocket();
+//    private WebSocket websocket = new WebSocket();
 
     @RequestMapping(value = "/index")
     public String index(String str) {
@@ -44,12 +39,12 @@ public class TestController {
 
 
 
-        @RequestMapping("/test")
-        public void sendMsg(HttpServletRequest request, HttpServletResponse response) throws IOException {
-            JSONObject json = new JSONObject();
-            json.put("to", request.getSession().getId());
-            json.put("msg", "欢迎连接WebSocket！！！！");
-            websocket.onMessage(json.toJSONString());
-
-    }
+//        @RequestMapping("/test")
+//        public void sendMsg(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//            JSONObject json = new JSONObject();
+//            json.put("to", request.getSession().getId());
+//            json.put("msg", "欢迎连接WebSocket！！！！");
+//            websocket.onMessage(json.toJSONString());
+//
+//    }
 }
