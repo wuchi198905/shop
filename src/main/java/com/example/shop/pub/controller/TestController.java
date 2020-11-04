2b81382786e36f256c2944b92f3da3fa66ad8288
倔强的萝卜 <769729374@qq.com>
2020-11-04 14:59:05 +0800

    增加mq的配置

--- src/main/java/com/example/shop/pub/controller/TestController.java
+++ src/main/java/com/example/shop/pub/controller/TestController.java
@@ -2,11 +2,18 @@ package com.example.shop.pub.controller;
 
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.shop.base.rabbitmq.MessageProducer;
+import com.example.shop.base.web.WebSocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -14,12 +21,35 @@ public class TestController {
 
     @Autowired
     private MessageProducer messageProducer;
+    private WebSocket websocket = new WebSocket();
 
     @RequestMapping(value = "/index")
     public String index(String str) {
         // 将实体实例写入消息队列
-        messageProducer.sendMessage(str);
+        Map<String,Object>map= new HashMap<>();
+        map.put("str",str);
+        messageProducer.sendMessage(map.toString());
         return "Success";
     }
+    /**
+     * 服务端推送消息对客户端
+     * @ClassName: ServiceClientController
+     * @Description: TODO
+     * @author OnlyMate
+     * @Date 2018年8月16日 下午2:45:22
+     *
+     */
 
+
+
+
+
+        @RequestMapping("/test")
+        public void sendMsg(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            JSONObject json = new JSONObject();
+            json.put("to", request.getSession().getId());
+            json.put("msg", "欢迎连接WebSocket！！！！");
+            websocket.onMessage(json.toJSONString());
+
+    }
 }
